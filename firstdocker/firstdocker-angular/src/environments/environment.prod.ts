b77de7e63c349b0e63ff52e3d20d7692b23b0170
badten91 <badten91@localhost>
2019-02-04 20:34:50 +0100

    latest version of the application

--- firstdocker/firstdocker-angular/src/environments/environment.prod.ts
+++ firstdocker/firstdocker-angular/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+import { HttpHeaders } from '@angular/common/http';
+
 export const environment = {
-  production: true
+  production: true,
+  endpoint:'http://localhost:8088/member/',
+  httpOptions: {
+    headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    }
 };
